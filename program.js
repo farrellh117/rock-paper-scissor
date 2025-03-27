c5cfc30bc5c01e46b8f9b39babdb96c31e200635
farrellh117 <farrelhafidi035@gmail.com>
2025-03-27 23:52:35 +0700

    refaktor function getPlayerChoice dan playRound

--- program.js
+++ program.js
@@ -1,71 +1,68 @@
-function getComputerChoice() {
-    let randomizeChoice = Math.random();
-
-    if(randomizeChoice < 0.33) {
-        return "gunting";
-    } else if(randomizeChoice < 0.66) {
-        return "batu";
-    } else {
-        return "kertas";
-    }
-}
-
-function getHumanChoice() {
-    const gunting = document.querySelector("#scissor-btn");
-    const batu = document.querySelector("#rock-btn");
-    const kertas = document.querySelector("#paper-btn");
-    
-    gunting.addEventListener("click", () => playRound("gunting", getComputerChoice()));
-    batu.addEventListener("click", () => playRound("batu", getComputerChoice()));
-    kertas.addEventListener("click", () => playRound("kertas", getComputerChoice()));
-}
-
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice === "gunting") {
-        if(computerChoice === "gunting") {
-            return "Seri";
-        } else if (computerChoice === "batu") {
-            ++computerScore;
-            return "Bot memang!";
-        } else {
-            ++humanScore;
-            return "Kamu menang!";
-        }
-    } else if (humanChoice === "batu") {
-        if(computerChoice === "batu") {
-            return "Seri";
-        } else if (computerChoice === "kertas") {
-            ++computerScore;
-            return "Bot memang!";
-        } else {
-            ++humanScore;
-            return "Kamu menang!";
-        }
-    } else if (humanChoice === "kertas") {
-        if(computerChoice === "kertas") {
-            return "Seri";
-        } else if (computerChoice === "gunting") {
-            ++computerScore;
-            return "Bot memang!";
-        } else {
-            ++humanScore;
-            return "Kamu menang!";
-        }
-    } else {
-        return "Sepertinya pilihan tersebut tidak ada. Tolong pilih gunting, batu, atau kertas!";
-    }
-}
-
-function playGame() {
-    // for(let i = 1; i <= 5; i++) {
-        // console.log(playRound(getHumanChoice(), getComputerChoice()));
-    // }
-
-    console.log("Skor kamu: " + humanScore);
-    console.log("Skor bot: " + computerScore);
-}
-
-let humanScore = 0;
+const choices = ["batu", "gunting", "kertas"];
+const playerDisplay = document.getElementById("player-display");
+const computerDisplay = document.getElementById("computer-display");
+const resultDisplay = document.getElementById("result-display");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
 let computerScore = 0;
 
-playGame();
+function getPlayerChoice() {
+    const gunting = document.getElementById("scissor-btn");
+    const batu = document.getElementById("rock-btn");
+    const kertas = document.getElementById("paper-btn");
+
+    gunting.addEventListener("click", function() {
+        playRound("gunting");
+    });
+
+    batu.addEventListener("click", function() {
+        playRound("batu");
+    });
+
+    kertas.addEventListener("click", function() {
+        playRound("kertas");
+    });
+}
+
+function playRound(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerChoice === computerChoice) {
+        result = "SERI!";
+    } else {
+        switch(playerChoice) {
+            case "batu":
+                result = (computerChoice === "gunting") ? "KAMU MENANG!" : "KAMU KALAH!";
+                break;
+            case "kertas":
+                result = (computerChoice === "batu") ? "KAMU MENANG!" : "KAMU KALAH!";
+                break;
+            case "gunting":
+                result = (computerChoice === "kertas") ? "KAMU MENANG!" : "KAMU KALAH!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`; 
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText", "redText");
+
+    switch(result) {
+        case "KAMU MENANG!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "KAMU KALAH!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+    }
+}
+
+getPlayerChoice();
